ab4cfde8cd2994802b2bf8d9cc44c3fce75aebae
dengue8830 <dengue8830@gmail.com>
2022-05-02 22:56:22 -0300

    hit the nail on builder pattern

--- builder/index.ts
+++ builder/index.ts
@@ -1,60 +1,125 @@
-// Requirements: the object need to be configured in a defer way
-// from different places in different moments.
+// ℹ️ Problem: you need to create different configurations of the same model.
+// ❌ Wrong interpretation: requiring subclasses. No, it's not a new class because
+// is the same group of attributes, responsabilities and behaviour.
+// Having restrictions for specific configurations doesn't necessarly implies subclasses.
 
-import { SimpleNotificationBuilder } from "./builder";
-import { director } from "./director";
-import { PlayerNotification, SimpleNotification } from "./model";
-import { preferences } from "./utils";
+// ❌ 1st try: place the code directly into the client.
+// Problem: That will be impossible to scale, mantain and re use. Should be only your first try.
+// const robot = new Robot();
+// robot.arms = 3;
+// robot.legs = 3;
+// if (robot.arms == 0) {
+//   throw new Error('The robot needs at least 1 arm to equip weapons');
+// }
 
-// Problem: repetitive setup code.
-const simpleNotif = new SimpleNotification();
-simpleNotif.icon = preferences.appIcon;
-simpleNotif.title = preferences.appName;
-simpleNotif.subtitle = "Message from Jhon";
-simpleNotif.show();
+export class Robot {
+  arms: number;
+  legs: number;
+  weapons: number;
+  turnOn() {}
 
-const playerNotif = new PlayerNotification();
-playerNotif.icon = preferences.appIcon;
-playerNotif.title = preferences.appName;
-playerNotif.subtitle = "Message from Laura";
-playerNotif.state = "paused";
-playerNotif.coverImage = "...";
-playerNotif.show();
+  // ❌ 2nd try: place that logic in the model.
+  // Problem: Creating specific a robot with an configuration but receiving params
+  // will make this impossible to scale and mantain due to the multiple
+  // combinations.
+  createTripoidRobot(weapons: number) {
+    const robot = new Robot();
+    robot.arms = 3;
+    robot.legs = 3;
+    if (robot.arms == 0) {
+      throw new Error("The robot needs at least 1 arm to equip weapons");
+    } else {
+      robot.weapons = weapons;
+    }
+    return robot;
+  }
 
-// Solution: Builder
-new SimpleNotificationBuilder()
-  .addIcon()
-  .addTitle()
-  .addSubtitle("message from Jhon")
-  .setWarningStyle()
-  .build()
-  .show();
+  // ❌
+  createBipodRobot(weapons: number) {
+    const robot = new Robot();
+    robot.arms = 3;
+    robot.legs = 2;
+    if (robot.arms == 0) {
+      throw new Error("The robot needs at least 1 arm to equip weapons");
+    } else {
+      robot.weapons = weapons;
+    }
+    return robot;
+  }
+}
 
-new SimpleNotificationBuilder()
-  .addIcon()
-  .addTitle()
-  .addSubtitle("Ups file upload failed")
-  .setErrorStyle()
-  .build()
-  .show();
+// ❌ 3rd try: directly with the director.
+// Problem: Same problem but with a cleaner model class.
+const director = {
+  createTripoidRobot(weapons: number) {
+    const robot = new Robot();
+    robot.arms = 3;
+    robot.legs = 3;
+    // ❗️ Check this custom logic in the middle of your config steps.
+    if (robot.arms == 0) {
+      throw new Error("The robot needs at least 1 arm to equip weapons");
+    } else {
+      robot.weapons = weapons;
+    }
+    return robot;
+  },
 
-// Even better: Director. if you have too much steps and those are repetitive on each specific config lets
-// abstract those steps in a director.
-director
-  .createWarningNotification()
-  .addSubtitle("message from Jhon")
-  .build()
-  .show();
+  createBipodRobot(weapons: number) {
+    const robot = new Robot();
+    robot.arms = 3;
+    robot.legs = 3;
+    if (robot.arms == 0) {
+      throw new Error("The robot needs at least 1 arm to equip weapons");
+    } else {
+      robot.weapons = weapons;
+    }
+    return robot;
+  },
+};
 
-director
-  .createErrorNotification()
-  .addSubtitle("Ups file upload failed")
-  .build()
-  .show();
+// ✅ 4th try: modularize the configuration into unordered steps.
+class RobotBuilder {
+  private robot: Robot = new Robot();
 
-director.createPremiumPlayerNotification().build().show();
+  addArms(arms: number) {
+    this.robot.arms = arms;
+    return this;
+  }
 
-/** Conclussion:
- * Most of the time this will be an overkill. KISS: Use just the director
- * and add abstraction layers as necessary.
- */
+  addLegs(legs: number) {
+    this.robot.legs = legs;
+    return this;
+  }
+
+  addWeapons(weapons: number) {
+    // ✅ Now your logic doesn't corrupt the different step combinations.
+    if (this.robot.arms == 0) {
+      throw new Error("The robot needs at least 1 arm to equip weapons");
+    } else {
+      this.robot.weapons = weapons;
+    }
+    return this;
+  }
+
+  build() {
+    // ✅ You can even check for a "ready" condition.
+    if (!this.robot.arms || !this.robot.legs || !this.robot.weapons) {
+      throw new Error("Your robot is incomplete!");
+    }
+    return this.robot;
+  }
+}
+
+// ✅ Improve: abstract common robot pre-configuration. (Optional)
+const correctDirector = {
+  createTripoidRobot() {
+    return new RobotBuilder().addArms(3).addLegs(3).addWeapons(2);
+  },
+
+  createBipodRobot() {
+    return new RobotBuilder().addArms(3).addLegs(2).addWeapons(2);
+  },
+};
+
+correctDirector.createTripoidRobot().build().turnOn();
+correctDirector.createBipodRobot().addArms(1).build().turnOn();
